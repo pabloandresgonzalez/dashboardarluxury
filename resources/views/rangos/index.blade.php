78c73bd14e123eef366ae5e764ceec61a40b1390
Pablo Andres Gonzalez <pabloandres6@gmail.com>
2021-08-22 19:15:38 -0500

    Edición índex membresías

--- resources/views/rangos/index.blade.php
+++ resources/views/rangos/index.blade.php
@@ -28,11 +28,11 @@
                 <div class="card" style="width: 18rem;">
                   <img class="card-img-top" src="{{ asset('img/brand/membresia1.PNG') }}" alt="Card image cap">
                   <div class="card-body" style="text-align: center;">
-                    <h4 class="card-title">100 Usd</h4> 
+                    <h4 class="card-title">REQUISITO:</h4>
                     <span> <p class="card-text">
-                    50 puntos<br></p></span><br>
-                    <h5 class="card-title">5 % de Administración</h5>                   
-                    <a href="#" class="btn btn-outline-success btn-lg btn-block">Comprar</a><br>  
+                    Quien compra un paquete de cualquier valor.<br>
+                    Tener un paquete activo.</p></span><br>
+                    <a href="#" class="btn btn-outline-success btn-lg btn-block">Comprar</a><br>
                   </div>
                 </div>
               </div>
@@ -43,11 +43,15 @@
                 <div class="card" style="width: 18rem;">
                   <img class="card-img-top" src="{{ asset('img/brand/membresia2.PNG') }}"  alt="Card image cap">
                   <div class="card-body" style="text-align: center;">
-                    <h4 class="card-title">250 Usd</h4> 
+                    <h3 class="card-title" >BENEFICIO</h3>
+                    <span> <p class="card-text">Bono de 50 Usd en USDT.</p></span><br>
+                    <h4 class="card-title">REQUISITO:</h4>
                     <span> <p class="card-text">
-                    125 puntos<br></p></span><br>
-                    <h5 class="card-title">5 % de Administración</h5>                   
-                    <a href="#" class="btn btn-outline-success btn-lg btn-block">Comprar</a><br>
+                    Tener en su equipo de ventas 5<br>
+                    Tener un paquete activo.</p></span><br>
+                    <a href="#" class="btn btn-outline-secondary btn-lg btn-block">Comprar</a>
+                     <hr class="my-3">
+                     RECALIFICACIÓN: 4 MESES Evento de capacitación<br>
                   </div>
                 </div>
               </div>
@@ -58,11 +62,17 @@
                 <div class="card" style="width: 18rem;">
                   <img class="card-img-top" src="{{ asset('img/brand/membresia3.PNG') }}" alt="Card image cap">
                   <div class="card-body" style="text-align: center;">
-                    <h4 class="card-title">500 Usd</h4> 
+                    <h3 class="card-title" >BENEFICIO</h3>
+                    <span> <p class="card-text">Participación seminario de 3 días en ciudad principal.
+                      <br>Bono de 200 Usd en USDT.</p></span><br>
+                    <h4 class="card-title">REQUISITO:</h4>
                     <span> <p class="card-text">
-                    250 puntos<br></p></span><br>
-                    <h5 class="card-title">5 % de Administración</h5>                     
-                    <a href="#" class="btn btn-outline-success btn-lg btn-block">Comprar</a><br>
+                    Tener en su equipo de ventas 2 Director y 30.000 Pts de V.V.<br>
+                    Ó 45.000 Pts de V.V.<br>
+                    Tener un paquete activo.</p></span><br>
+                    <a href="#" class="btn btn-outline-primary btn-lg btn-block">Comprar</a>
+                     <hr class="my-3">
+                     RECALIFICACIÓN: 4 MESES Evento de capacitación<br>
                   </div>
                 </div>
               </div>
@@ -84,11 +94,16 @@
                 <div class="card" style="width: 18rem;">
                   <img class="card-img-top" src="{{ asset('img/brand/membresia4.PNG') }}" alt="Card image cap">
                   <div class="card-body" style="text-align: center;">
-                    <h4 class="card-title">1000 Usd</h4> 
+                    <h3 class="card-title" >BENEFICIO</h3>
+                    <span> <p class="card-text">Bono de 300 Usd en USDT.</p></span><br>
+                    <h4 class="card-title">REQUISITO:</h4>
                     <span> <p class="card-text">
-                    500 puntos<br></p></span><br>
-                    <h5 class="card-title">5 % de Administración</h5>                    
-                    <a href="#" class="btn btn-outline-success btn-lg btn-block">Comprar</a><br>
+                    Tener en su equipo de ventas 2 Perla y 60.000 Pts de V.V.<br>
+                    Ó 90.000 Pts de V.V.<br>
+                    Tener un paquete activo.</p></span><br>
+                    <a href="#" class="btn btn-outline-success btn-lg btn-block">Comprar</a>
+                     <hr class="my-3">
+                     RECALIFICACIÓN: 4 MESES Evento de capacitación + Netbook<br>
                   </div>
                 </div>
               </div>
@@ -99,11 +114,16 @@
                 <div class="card" style="width: 18rem;">
                   <img class="card-img-top" src="{{ asset('img/brand/membresia5.PNG') }}"  alt="Card image cap">
                   <div class="card-body" style="text-align: center;">
-                    <h4 class="card-title">2000 Usd</h4> 
+                    <h3 class="card-title" >BENEFICIO</h3>
+                    <span> <p class="card-text">Bono de 400 Usd en USDT.</p></span><br>
+                    <h4 class="card-title">REQUISITO:</h4>
                     <span> <p class="card-text">
-                    1000 puntos<br></p></span><br>
-                    <h5 class="card-title">5 % de Administración</h5>                     
-                    <a href="#" class="btn btn-outline-success btn-lg btn-block">Comprar</a><br>
+                    Tener en su equipo de ventas 2 Óro. y 180.000 Pts de V.V.<br>
+                    Ó 330.000 Pts de V.V.<br>
+                    Tener un paquete activo.</p></span><br>
+                    <a href="#" class="btn btn-outline-success btn-lg btn-block">Comprar</a>
+                     <hr class="my-3">
+                     RECALIFICACIÓN: 4 MESES Evento de capacitación + Macbook<br>
                   </div>
                 </div>
               </div>
@@ -114,11 +134,16 @@
                 <div class="card" style="width: 18rem;">
                   <img class="card-img-top" src="{{ asset('img/brand/membresia6.PNG') }}" alt="Card image cap">
                   <div class="card-body" style="text-align: center;">
-                    <h4 class="card-title">2000 Usd</h4> 
+                    <h3 class="card-title" >BENEFICIO</h3>
+                    <span> <p class="card-text">Bono de liderazgo de 25.0000 Usd en BTC.</p></span><br>
+                    <h4 class="card-title">REQUISITO:</h4>
                     <span> <p class="card-text">
-                    1000 puntos<br></p></span><br>
-                    <h5 class="card-title">5 % de Administración</h5>                    
-                    <a href="#" class="btn btn-outline-success btn-lg btn-block">Comprar</a><br>
+                    Tener en su equipo de ventas 2 Rubí. Y 360.000 Pts de V.V.<br>
+                    Ó más de 1'400.000 Pts de V.V.<br>
+                    Tener un paquete activo.</p></span><br>
+                    <a href="#" class="btn btn-outline-success btn-lg btn-block">Comprar</a>
+                     <hr class="my-3">
+                     RECALIFICACIÓN: 4 MESES Evento de capacitación Internacional<br>
                   </div>
                 </div>
               </div>
@@ -139,39 +164,25 @@
               <div class="row">
                 <div class="card" style="width: 18rem;">
                   <img class="card-img-top" src="{{ asset('img/brand/membresia7.PNG') }}" alt="Card image cap">
-                  <div class="card-body" style="text-align: center;">                    
-                    <h4 class="card-title">3000 Usd</h4> 
-                    <span> <p class="card-text">
-                    1500 puntos<br></p></span><br>
-                    <h5 class="card-title">5 % de Administración</h5>                   
-                    <a href="#" class="btn btn-outline-success btn-lg btn-block">Comprar</a><br>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-xl-4 col-lg-6">
-              <div class="row">
-                <div class="card" style="width: 18rem;">
-                  <img class="card-img-top" src="{{ asset('img/brand/membresia6.PNG') }}" alt="Card image cap">
                   <div class="card-body" style="text-align: center;">
-                    <h4 class="card-title">5000 Usd</h4> 
+                    <h3 class="card-title" >BENEFICIO</h3>
+                    <span> <p class="card-text">Bono de liderazgo de 50.000 Usd.</p></span><br>
+                    <h4 class="card-title">REQUISITO:</h4>
                     <span> <p class="card-text">
-                    2500 puntos<br></p></span><br>
-                    <h5 class="card-title">5 % de Administración</h5>                    
-                    <a href="#" class="btn btn-outline-success btn-lg btn-block">Comprar</a><br>
+                    Tener en su equipo de ventas 2 Esmeralda. Y 900.000 Pts de V.V.<br>
+                    Ó más de 2'000.000 Pts de V.V.<br>
+                    Tener un paquete activo.</p></span><br>
+                    <a href="#" class="btn btn-outline-success btn-lg btn-block">Comprar</a>
+                     <hr class="my-3">
+                     RECALIFICACIÓN: 4 MESES Evento de capacitación Internacional<br>
                   </div>
                 </div>
               </div>
             </div>
-
 
           </div>
         </div>
       </div>
-      <br>
-        <hr class="my-3">
-           <h5> &nbsp;De la misma forma que pagues tu paquete, así mismo será tu pago. Ejemplo: si compras con Dash tus pagos serán en Dash<br><h5/>
 
         <main class="py-4">
             @yield('content')
