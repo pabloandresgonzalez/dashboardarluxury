d292a44839cc988748143076f4dfd908e5a79138
pabloandres <pabloandres6@gmail.com>
2021-09-25 14:09:06 -0500

    Migracion relacion network_transactions.userMembership con user_memberships.id

--- resources/views/news/indexuser.blade.php
+++ resources/views/news/indexuser.blade.php
@@ -38,7 +38,7 @@
                         </div>
                     </div>
 
-                    @if(in_array(strtolower(pathinfo($new->image,PATHINFO_EXTENSION)),["png","jpg","jpeg"]))
+                    @if(in_array(strtolower(pathinfo($new->image,PATHINFO_EXTENSION)),["png","jpg","gif","avg"]))
                     <div class="card-body" style="text-align:center;"> 
                         <img class="table-responsive" width="240" height="320" controls src="{{ route('new.avatar',['filename'=>$new->image]) }}"> 
                     </div>
