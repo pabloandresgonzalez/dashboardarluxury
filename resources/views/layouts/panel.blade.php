8507f0bca196f9e5f3bd3b795f023833a0388f93
pabloandres6 <pabloandres6@gmail.com>
2022-08-06 09:57:12 -0500

    Ajuste Slider

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -151,7 +151,8 @@
     <nav class="navbar navbar-top navbar-expand-md navbar-dark" id="navbar-main">
       <div class="container-fluid">
         <!-- Brand -->
-        <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="#">Panel</a>
+        <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="#">Panel </a>
+        <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="#"> &nbsp; &nbsp;</a>
 
           <!-- partial:index.partial.html -->
           <div class="slider">
